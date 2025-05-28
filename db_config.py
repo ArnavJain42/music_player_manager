6bef31ba0f13740c565237e7ac10f71f5f9bb46a
ARNAV JAIN <118369980+ArnavJain42@users.noreply.github.com>
2025-05-29 01:34:48 +0530

    python anywhere

--- db_config.py
+++ db_config.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="aj",
-        password="123",
-        database="music_db"
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE")
     )
